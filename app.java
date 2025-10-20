08fe11a6a5cf2e32366da8bfe1421249490c4230
Izan <izandemaya@gmail.com>
2025-10-20 16:34:06 +0200

    Creació de les variables globals

---
+++ app.java
@@ -0,0 +1,14 @@
+public class app {
+
+    //Variable global
+    boolean livingRoom;
+    boolean bathroom;
+    boolean kitchen;
+    boolean h1;
+    boolean h2;
+    boolean h3;
+    
+    public static void main(String[] args) {
+        
+    }
+}
