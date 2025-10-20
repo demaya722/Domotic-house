ddd901e9e08a4f18389fcc66f27de570f2ee6253
Izan <izandemaya@gmail.com>
2025-10-20 16:58:11 +0200

    Creació del menú per sel·leccionar un control domótico

--- app.java
+++ app.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class app {
 
     //Variable global
@@ -15,6 +17,40 @@ public class app {
         boolean temperature;
         boolean fan;
         boolean blinds;
+        int options = 0;
+        int menu = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("============MENÚ============");
+            System.out.println("1- Llums");
+            System.out.println("2- Calefacció");
+            System.out.println("3- Bentilador");
+            System.out.println("4- Finestres");
+
+            System.out.print("Escull una opció del 1 - 4: ");
+
+            try { //Per verificar q ha escolit correctament una opció
+                menu = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Error: Has d'introduir un número vàlid.");
+                 scanner.nextLine();
+                continue;
+            } finally {
+                scanner.nextLine();
+            }
+
+
+            switch (options) {
+                case 1:
+                    System.out.println("");
+                    break;
+            
+                default:
+                    break;
+            }
+        }
         
 
     }
